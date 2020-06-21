4bb123eca25953e8f4dbf84d62ce295ebdfac45a
Toni  Diep <adiep123@gmail.com>
2020-06-21 16:11:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+  "Hello, World!"
+  end
+  get '/name' do
+  "My name is Toni."
+  end
+   get '/hometown' do
+  "My hometown is the San Gabriel Valley."
+  end
+   get '/favorite-song' do
+  "My favorite song is Toni."
+  end
+  
 end
